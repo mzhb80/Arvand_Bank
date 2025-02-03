ff3e2b558b343fd4e82037246d552073d51a5bdd
zarif <mzhb13801398@gmail.com>
2025-02-03 21:35:06 +0330

    feat: :white_check_mark: create test page

---
+++ src/views/test.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>colors</h1>
+        <div class="flex flex-wrap">
+            <div class="w-100px h-100px bg-primary-50"></div>
+            <div class="w-100px h-100px bg-primary-100"></div>
+            <div class="w-100px h-100px bg-primary-200"></div>
+            <div class="w-100px h-100px bg-primary-300"></div>
+            <div class="w-100px h-100px bg-primary-400"></div>
+            <div class="w-100px h-100px bg-primary-500"></div>
+            <div class="w-100px h-100px bg-primary-600"></div>
+            <div class="w-100px h-100px bg-primary-700"></div>
+            <div class="w-100px h-100px bg-primary-800"></div>
+            <div class="w-100px h-100px bg-primary-900"></div>
+            <div class="w-100px h-100px bg-primary-950"></div>
+            <div class="w-100px h-100px bg-black"></div>
+            <div class="w-100px h-100px bg-gray-1"></div>
+            <div class="w-100px h-100px bg-gray-2"></div>
+            <div class="w-100px h-100px bg-gray-3"></div>
+            <div class="w-100px h-100px bg-gray-4"></div>
+            <div class="w-100px h-100px bg-gray-5"></div>
+            <div class="w-100px h-100px bg-gray-6"></div>
+            <div class="w-100px h-100px bg-gray-7"></div>
+            <div class="w-100px h-100px bg-gray-8"></div>
+            <div class="w-100px h-100px bg-gray-9"></div>
+            <div class="w-100px h-100px bg-gray-10"></div>
+            <div class="w-100px h-100px bg-gray-11"></div>
+            <div class="w-100px h-100px bg-white"></div>
+            <div class="w-100px h-100px bg-error-1"></div>
+            <div class="w-100px h-100px bg-error-2"></div>
+            <div class="w-100px h-100px bg-success-1"></div>
+            <div class="w-100px h-100px bg-success-2"></div>
+            <div class="w-100px h-100px bg-info-1"></div>
+            <div class="w-100px h-100px bg-info-2"></div>
+            <div class="w-100px h-100px bg-warning-1"></div>
+            <div class="w-100px h-100px bg-warning-2"></div>
+            <div class="w-100px h-100px bg-chart"></div>
+        </div>
+        <h1>Shadow</h1>
+        <div class="w-100px h-100px shadow"></div>
+        <h1>Typography</h1>
+        <p class="text-display-1">بانک اروند</p>
+        <p class="text-display-2">بانک اروند</p>
+        <p class="text-heading-1">بانک اروند</p>
+        <p class="text-heading-2">بانک اروند</p>
+        <p class="text-heading-3">بانک اروند</p>
+        <p class="text-heading-4">بانک اروند</p>
+        <p class="text-heading-5">بانک اروند</p>
+        <p class="text-heading-6">بانک اروند</p>
+        <p class="text-body-1">بانک اروند</p>
+        <p class="text-body-2">بانک اروند</p>
+        <p class="text-body-3">بانک اروند</p>
+        <p class="text-body-4">بانک اروند</p>
+        <p class="text-body-5">بانک اروند</p>
+        <p class="text-caption-1">بانک اروند</p>
+        <p class="text-caption-2">بانک اروند</p>
+        <p class="text-button-1">بانک اروند</p>
+        <p class="text-button-2">بانک اروند</p>
+        <p class="text-overline-1">بانک اروند</p>
+        <p class="text-overline-2">بانک اروند</p>
+    </div>
+</template>
