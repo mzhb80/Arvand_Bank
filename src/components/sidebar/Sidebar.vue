a402dc3efb3733e3cebd7b0889a328083b2afd10
zarif <mzhb13801398@gmail.com>
2025-02-04 20:31:14 +0330

    chore: :zap: dynamic colors

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -2,12 +2,23 @@
 import SidebarItem, { type SidebarItemType } from '@/components/sidebar/SidebarItem.vue';
 
 const sidebarItems: SidebarItemType[] = [
-    { text: 'داشبورد', link: '/', icon: '', showText: true },
+    { 
+        text: 'داشبورد',
+        link: '/',
+        icon: '',
+        showText: true,
+        subItems: [
+            { link: '/1', text: 'تست 1', },
+            { link: '/2', text: 'تست 2', },
+            { link: '/3', text: 'تست 3', },
+            { link: '/4', text: 'تست 4', },
+        ]
+    },
 ]
 </script>
 
 <template>
-    <div class="border-e-1">
+    <div class="border-e-1 p-6 h-100vh">
         <SidebarItem v-for="item, index in sidebarItems" :key="index" :icon="item.icon" :link="item.link"
             :show-text="item.showText" :sub-items="item.subItems" :text="item.text" />
     </div>
--- src/components/sidebar/SidebarItem.vue
+++ src/components/sidebar/SidebarItem.vue
@@ -9,6 +9,29 @@ export type SidebarItemType = {
         link: string
     }[]
 }
+
+const config = {
+    default: {
+        textColor: 'text-black',
+        iconColor: 'text-gray-4',
+        bgColor: '',
+    },
+    hover: {
+        textColor: 'text-primary-500',
+        iconColor: 'text-primary-500',
+        bgColor: 'bg-primary-50',
+    },
+    active: {
+        textColor: 'text-primary-50',
+        iconColor: 'text-primary-50',
+        bgColor: 'bg-primary-900',
+    },
+    'active-submenu': {
+        textColor: 'text-primary-500',
+        iconColor: 'text-primary-500',
+        bgColor: 'bg-primary-50',
+    },
+}
 </script>
 
 <script setup lang="ts">
@@ -19,14 +42,40 @@ type SidebarState = 'default' | 'hover' | 'active' | 'active-submenu'
 
 const props = defineProps<SidebarItemType>()
 
+const showSubItems = ref(false)
+
 const state = ref<SidebarState>('default')
 
-function handleClick() {}
+function handleClick() {
+    if (props.subItems?.length) showSubItems.value = !showSubItems.value
+ }
+
+function handleMouseEnter() {
+    if (state.value === 'default') state.value = 'hover'
+}
+
+function handleMouseLeave() {
+    if (state.value === 'hover') state.value = 'default'
+}
 </script>
 
 <template>
-    <button @click="handleClick" class="flex items-center px-3 py-10px">
-        <span class="ms-2 text-body-4">{{ text }}</span>
-        <BaseIcon src="@/assets/icons/angle-left-small.svg" class="s-6 ms-2" />
-    </button>
+    <div>
+        <button @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" @click="handleClick"
+            class="flex items-center px-3 py-10px rounded-2 transition-colors duration-500"
+            :class="[config[state].bgColor, config[state].textColor]">
+            <BaseIcon :src="props.icon" class="s-6" :class="config[state].iconColor" />
+            <span class="ms-2 text-body-4">{{ text }}</span>
+            <BaseIcon src="@/assets/icons/angle-left-small.svg" class="s-6 ms-2" :class="[config[state].iconColor, { 'rotate-90': showSubItems}]" />
+        </button>
+        <Transition name="fade">
+            <ul class="mt-2 pt-4 ps-4 pe-3 grid grid-cols-1 gap-y-3 list-disc" v-if="subItems && showSubItems">
+                <li v-for="item in subItems" :key="item.link" class="text-center text-button-2 text-gray-1">
+                    <router-link :to="item.link">
+                        {{ item.text }}
+                    </router-link>
+                </li>
+            </ul>
+        </Transition>
+    </div>
 </template>
